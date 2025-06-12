18e890eb13196615f3a4e81e145880b30bb87056
Anish Dattatraya Tabib <anish18ise@gmail.com>
2025-06-12 17:47:38 +0530

    Update email.config.js

--- Backend/nodemailer/email.config.js
+++ Backend/nodemailer/email.config.js
@@ -1,2 +1,14 @@
+import nodemailer from 'nodemailer'
+
+export const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true for port 465, false for other ports
+  auth: {
+    user: "Your email id",
+    pass: "Your password"
+  },
+});
+
 
 
